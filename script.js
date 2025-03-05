deef6b18c4ca0ef44d8c0f9bba3e21f1dba83b01
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-05 15:38:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 script.js doesn't contain: ["document.addEventListener", "DOMContentLoaded", "function"]
-  
+script.js doesn't contain: ["addButton", "add-task-btn", "taskInput", "task-input", "taskList", "task-list"]  
