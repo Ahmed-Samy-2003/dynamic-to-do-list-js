508e786e8131272b1404887392468d8486426504
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-05 15:39:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 script.js doesn't contain: ["document.addEventListener", "DOMContentLoaded", "function"]
 script.js doesn't contain: ["addButton", "add-task-btn", "taskInput", "task-input", "taskList", "task-list"]  
 script.js doesn't contain: ["addTask", "taskInput.value.trim()", "taskText", "alert"]
+script.js doesn't contain: ["document.createElement", "button", "Remove", "remove-btn", "classList.add", "appendChild"]
